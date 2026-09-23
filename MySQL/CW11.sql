e6ef2ee6c8ac9fcd86d60bff6f44b0e097b6333b
Anand <anandanil731@gmail.com>
2026-09-23 09:46:04 +0530

    html program codes

--- MySQL/CW11.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE cw11;
-USE cw11;
-CREATE TABLE authors(author_id INT(10) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(35));
-
-CREATE TABLE books(book_id INT(10) AUTO_INCREMENT PRIMARY KEY, title VARCHAR(35), author_id INT(10), FOREIGN KEY(author_id) REFERENCES authors(author_id));
-
-CREATE INDEX author_indx ON books(author_id);
